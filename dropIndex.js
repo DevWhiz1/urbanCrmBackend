fe7ce759698e19fa321988bff5e81ac5e1d8658c
DevWhiz1 <ahmadbajwa3095@gmail.com>
2026-08-04 18:51:04 +0500

    refactor: Remove legacy index dropping logic from database connection

--- dropIndex.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-    try { await mongoose.connection.collection('employees').dropIndex('employeeId_1'); console.log('Dropped employeeId_1'); } catch(e) { console.log(e.message); }
-    try { await mongoose.connection.collection('expenses').dropIndex('expenseId_1'); console.log('Dropped expenseId_1'); } catch(e) { console.log(e.message); }
-    console.log('Indexes dropped');
-    process.exit(0);
-});
